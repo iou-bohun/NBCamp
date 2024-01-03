6dbd362cb285cf79e07a648e67f6ffd5071b75f0
bohun <slawkd2002@gmail.com>
2024-01-03 20:40:59 +0900

    추가: 텍스트 RPG
    C# 콘솔 텍스트 RPG  제작

---
+++ TextRpg/TextRpg/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRpg
+{
+    public class Player
+    {
+        public int level = 0;
+        public string name = "IOU";
+        public string job = "전사";
+        public int atk = 10;
+        public int atkAfter = 0;
+        public int def = 5;
+        public int defAfter = 0;
+        public int health = 100;
+    }
+
+}
